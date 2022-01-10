6ae96e1bccf0031ced29963dfc18feac762d0274
dni <dni.khr@gmail.com>
2022-01-11 00:27:52 +0100

    add certbot and ssl certificate

--- server/install-mumble.sh
+++ server/install-mumble.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 apt-get update
-apt install -y mumble-server
+apt install -y mumble-server certbot
 ufw allow 64738/tcp
 ufw allow 64738/udp
 dpkg-reconfigure mumble-server
+echo "enter mumble domain"
+read -r domain
+certbot certonly --webroot --agree-tos --text --non-interactive --webroot-path /var/www/html -d $domain
 echo "enter mumble server password"
 read -r password
 cat <<EOF > /etc/mumble-server.ini
+sslCert=/etc/letsencrypt/live/$domain/fullchain.pem
+sslKey=/etc/letsencrypt/live/$domain/privkey.pem
 database=/var/lib/mumble-server/mumble-server.sqlite
 icesecretwrite=
 logfile=/var/log/mumble-server/mumble-server.log
