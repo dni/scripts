6d655b32b66359bc549d2680cbc837b837922fe0
dni <dni.khr@gmail.com>
2022-01-12 22:44:32 +0100

    add vim compiling

--- server/ubuntu.sh
+++ server/ubuntu.sh
@@ -29,3 +29,21 @@ echo "Europe/Vienna" > /etc/timezone
 
 # change shell
 # chsh -s zsh root
+
+## compile vim from source for vimspector python3.6 support
+apt install libncurses-dev python3-dev python3-distutils
+cd /tmp/
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+  --enable-multibyte \
+  --enable-rubyinterp=yes \
+  --enable-python3interp=yes \
+  --with-python3-config-dir=$(python3-config --configdir) \
+  --enable-perlinterp=yes \
+  --enable-luainterp=yes \
+  --enable-cscope \
+  --prefix=/usr/local
+make
+make install
+
