dc59e39d5a6c058a446e430534aa95aa31a24f5e
dni <dni.khr@gmail.com>
2022-01-13 01:26:46 +0100

    unfunny vim building for ubuntu

--- server/ubuntu.sh
+++ server/ubuntu.sh
@@ -30,15 +30,24 @@ echo "Europe/Vienna" > /etc/timezone
 # change shell
 # chsh -s zsh root
 
-## compile vim from source for vimspector python3.6 support
-apt install libncurses-dev python3-dev python3-distutils
+## python3.8 support and ubuntu magic
+apt install libncurses-dev python3.8-dev python3-distutils
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+update-alternatives --install /usr/bin/python3-config python3-config /usr/bin/python3.6-config 1
+update-alternatives --install /usr/bin/python3-config python3-config /usr/bin/python3.8-config 2
+update-alternatives --config python3
+update-alternatives --config python3-config
+
+# compile vim with python3.8 support
 cd /tmp/
 git clone https://github.com/vim/vim.git
 cd vim
-./configure --with-features=huge \
+LDFLAGS="-rdynamic" ./configure --with-features=huge \
   --enable-multibyte \
   --enable-rubyinterp=yes \
   --enable-python3interp=yes \
+  --with-python3-command=python3.8 \
   --with-python3-config-dir=$(python3-config --configdir) \
   --enable-perlinterp=yes \
   --enable-luainterp=yes \
@@ -46,4 +55,5 @@ cd vim
   --prefix=/usr/local
 make
 make install
-
+echo ":VimspectorInstall vscode-php-debug"
+echo "for xcode debugging"
