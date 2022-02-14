640d80d335a1521ea10e18dde02bd662a21d3edd
dni <dni.khr@gmail.com>
2022-02-14 09:31:09 +0100

    fix username

--- server/ubuntu/user.sh
+++ server/ubuntu/user.sh
@@ -5,8 +5,8 @@ if [ ! -z $username ] && [ ! "$username" = "typo3" ] && [ ! "$username" = "magen
 fi
 
 useradd -G www-data $username
-mkdir -p /home/#{username}/.composer
-mkdir -p /home/#{username}/.aws
+mkdir -p /home/$username/.composer
+mkdir -p /home/$username/.aws
 mkdir -p /home/$username/.ssh
 chown -R $username /home/$username/
 chown -R $username:www-data /var/www
