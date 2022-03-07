412456f531dd38a32bb34ba4f62aa7686d2e5501
dni <dni.khr@gmail.com>
2022-03-07 09:54:13 +0100

    fix script error in magento2 crontab script

--- server/templates/magento2/crontab.sh
+++ server/templates/magento2/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for f in $(ls /var/www/); do
-  if [ ! "$f" == "html" ]; then
+  if [ ! "$f" = "html" ]; then
     /usr/bin/flock -n /var/www/$f/.cron.flag test ! -e /var/www/$f/var/.maintenance.flag \
       && /usr/bin/php /var/www/$f/bin/magento cron:run | \
       grep -v 'Ran jobs by schedule' >> /var/www/$f/var/log/magento.cron.log
