2424ee8a50ed54c417b3b3a1c4709469363132b3
dni <dni.khr@gmail.com>
2022-03-20 01:36:32 +0100

    dont press enter :)

--- server/ubuntu/php.sh
+++ server/ubuntu/php.sh
@@ -9,8 +9,8 @@ fi
 echo "##### php.sh START #####"
 
 # install custom repo for php
-add-apt-repository ppa:ondrej/apache2
-add-apt-repository ppa:ondrej/php
+add-apt-repository -y ppa:ondrej/apache2
+add-apt-repository -y ppa:ondrej/php
 apt-get update > /dev/null
 
 apt-get install -y php${php_version} php${php_version}-dev php${php_version}-apcu php${php_version}-gd php${php_version}-mysqli php${php_version}-mbstring php${php_version}-soap php${php_version}-zip php${php_version}-xml php${php_version}-bcmath php${php_version}-curl php${php_version}-intl > /dev/null
