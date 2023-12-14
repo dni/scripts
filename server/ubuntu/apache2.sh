9b8b305c437bc4b537a4eaa08710aff6e392a102
dni ⚡ <office@dnilabs.com>
2023-12-14 23:43:32 +0100

    update scripts

--- server/ubuntu/apache2.sh
+++ server/ubuntu/apache2.sh
@@ -2,8 +2,9 @@
 
 echo "##### apache2.sh START #####"
 
-apt-get install -y apache2 > /dev/null
-a2enmod rewrite headers expires ssl proxy proxy_http
+apt-get install -y apache2 libapache2-mod-fcid > /dev/null
+a2enmod rewrite headers expires ssl proxy proxy_http proxy_fcgi setenvif
+a2enconf php8.2-fpm
 
 # seo
 echo 'ServerTokens Prod' >> /etc/apache2/apache2.conf
@@ -14,7 +15,7 @@ cp /root/scripts/server/templates/blacklist.conf /etc/apache2/blacklist.conf
 
 # landing page
 rm -rf /var/www/html
-git clone git@git.dnilabs.com:landing.git /var/www/html
+git clone https://github.com:dni/landing.git /var/www/html
 chown -R www-data:www-data /var/www/html
 
 echo "##### apache2.sh END #####"
--- server/ubuntu/php.sh
+++ server/ubuntu/php.sh
@@ -13,13 +13,15 @@ add-apt-repository -y ppa:ondrej/apache2
 add-apt-repository -y ppa:ondrej/php
 apt-get update > /dev/null
 
-apt-get install -y php${php_version} php${php_version}-dev php${php_version}-apcu php${php_version}-gd php${php_version}-mysqli php${php_version}-mbstring php${php_version}-soap php${php_version}-zip php${php_version}-xml php${php_version}-bcmath php${php_version}-curl php${php_version}-intl > /dev/null
+apt-get install -y php${php_version} php${php_version}-dev  php${php_version}-fpm php${php_version}-apcu php${php_version}-gd php${php_version}-mysqli php${php_version}-mbstring php${php_version}-soap php${php_version}-zip php${php_version}-xml php${php_version}-bcmath php${php_version}-curl php${php_version}-intl > /dev/null
 
 update-alternatives --set php /usr/bin/php${php_version}
 
 phpenmod -v $php_version opcache apcu
 a2dismod php5
 a2enmod php${php_version}
+#locale-gen de_DE.UTF-8
+#update-locale LANG=de_DE.UTF-8 LC_MESSAGES=POSIX
 
 # install composer
 curl -sS https://getcomposer.org/installer | php
